e58ecd6d93781dd628e16d1d61f7dbcb4e1bcd4e
FrancaIsis <isisdej@outlook.com>
2024-04-12 21:21:46 -0300

    ola mundo

---
+++ aula001_primeiro_programa/ola_mundo.py
@@ -0,0 +1,22 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152 (a Braba)
+# Autor: Isis França
+# Data: 12/04/2024
+# Primeiro Programa em Python
+
+# Importando as bibliotecas
+import os
+
+
+# Limpando o terminal
+os.system('cls')
+
+# O comando print() executa uma saída via terminal
+
+print('-'*70)
+print('Hello World!!!')
+print('-'*70)
+print()
+
+
+
