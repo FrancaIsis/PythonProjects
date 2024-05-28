ca78f7631de5608b3a2fe1b9e5c5ff9083493c54
FrancaIsis <isisdej@outlook.com>
2024-05-27 21:40:48 -0300

    teste

---
+++ aula007_repeticoes/exemplo002.py
@@ -0,0 +1,16 @@
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('ESTRUTURA DE CONTROLE FOR RANGE')
+print('='*70)
+
+print()
+
+for var_iteradora in range(1, 5):
+    cor = str(input(f'Digite a {var_iteradora}º cor: ')) # para cada variavel iteradora dentro do alcance (inicio, fim)
+    
+print()
+print()
