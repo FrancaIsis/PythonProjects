5a5f2d3fb2cfe56f9298e16a79b426753dabc109
FrancaIsis <isisdej@outlook.com>
2024-07-06 13:04:48 -0300

    .

--- atividade009/letra_h.py
+++ atividade009/letra_h.py
@@ -7,3 +7,28 @@
 # Em seguida realize as seguintes ações:
 # - Listar chaves e valores com laço - Deletar o peso
 # - Listar novamente chaves e valores - mostrar o nome e altura
+
+import os
+
+
+os.system('cls')
+
+print('-' * 70)
+print('DICIONÁRIO')
+print('=' * 70)
+
+nomes = {'nome': 'John', 'idade': 40, 'peso': 80, 'altura': 1.70}
+
+#mostrando os valores com laço
+for chave, valor in nomes.items():
+    print(f'{chave}:{valor}')
+
+# deletando o peso
+del nomes['peso']
+print('-'*70)
+
+#mostrando apenas nome e altura
+for chave, valor in nomes.items():
+    if chave == 'idade':
+        continue
+    print(f'{chave}:{valor}')
